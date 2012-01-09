448433cb95701cc6ac39a904347c6138e6cdb37d
Marconi Moreto <caketoad@gmail.com>
2012-01-10 02:49:32 +0800

    Initial Commit

---
+++ FlightPing/FlightPingAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FlightPingAppDelegate.h
+//  FlightPing
+//
+//  Created by Marconi Moreto on 1/10/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlightPingAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
